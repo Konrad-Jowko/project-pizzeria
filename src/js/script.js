1f43a8a5551c48d6be3397e789e3b571cac70beb
Konrad Jówko <konrad.jowko@gmail.com>
2021-05-04 19:43:17 +0200

    Add showing/hiding shopping cart

--- src/js/script.js
+++ src/js/script.js
@@ -138,6 +138,38 @@ class AmountWidget {
 
 }
 
+class Cart{
+  constructor(element){
+    const thisCart = this;
+
+    thisCart.products = [];
+
+    thisCart.getElements(element);
+    thisCart.initActions();
+    console.log('new Cart:', thisCart);
+
+  }
+  getElements(element){
+    const thisCart = this;
+
+    thisCart.dom = {};
+
+    thisCart.dom.wrapper = element;
+    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+
+  }
+  initActions(){
+    const thisCart = this;
+
+    thisCart.dom.toggleTrigger.addEventListener('click', function(){
+    thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+
+    });
+
+  };
+
+}
+
 const app = {
 
   initData: function(){
@@ -156,6 +188,12 @@ const app = {
 
 
   },
+  initCart: function(){
+    const thisApp = this;
+
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+  },
 
   init: function(){
     const thisApp = this;
@@ -167,7 +205,9 @@ const app = {
 
     thisApp.initData();
     thisApp.initMenu();
+    thisApp.initCart();
   },
+
 };
 
 class Product{
